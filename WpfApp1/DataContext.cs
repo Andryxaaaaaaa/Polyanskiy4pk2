743800c26202224a334929a5b23ab93cd47c54ee
polyanskiy.av1267 <polyanskiy.av1267@COM409-L-2>
2023-11-27 15:36:52 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+     internal class DataContext : DbContext 
+    {
+        //public DataContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=192.168.147.50\\MSSQL2;Initial Catalog=База_ДанныхИу;User ID=pk;Password=1;");
+        }
+        public DbSet <Income> Income { get; set; }
+    }
+}
---
+++ WpfApp1/Income.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Income
+    {
+        [Key] 
+        public int code { get; set; }
+
+        public byte point { get; set; }
+        public DateTime date { get; set; }
+        public decimal inc { get; set; }
+    }
+}
